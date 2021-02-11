1ed8855557c25edbf4a94358a29565b2c0c732ea
AlejoOspina01 <alejandro.ospina@correounivalle.edu.co>
2021-02-11 11:37:05 -0500

    index mensajes

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bienvenido</title>
+</head>
+<body>
+
+	<h1>Enviar un correo</h1>
+	<hr>
+	<h3>Ingresa un mensaje para enviar</h1>
+		<input type="text" name="mensaje">
+
+	</body>
+	</html>
